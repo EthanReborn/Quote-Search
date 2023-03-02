c85011460895b26ddd6792703d19b4a6bedd9f69
Ethan <A02314504@aggies.usu.edu>
2023-03-02 16:12:08 -0700

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,46 @@
-import { SyntheticEvent, useState, useEffect, Component } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
+import { Root } from "./pages/rootNotes"
+import { User } from "./pages/User";
+import { Home } from "./pages/Home";
+import { useState } from "react";
+import { Api } from "./lib/api";
+import { ApiContext } from "./contexts/api";
 
-interface Quote {
-  _id: string
-  content: string
-  author: string
-}
-
-export function App() {
-  const [quote, setQuote] = useState<Quote | null>(null); //null for object types 
-  const [author, setAuthor] = useState("");
-  const [quotes, setQuotes] = useState<Quote[]>([]);
-  const [hasSearched, setHasSearched] = useState(false);
-  const className = hasSearched ? "top-container" : "center-container";
-
-  async function search(e: SyntheticEvent) {
-    setHasSearched(true);
-    e.preventDefault();
-    let quoteList: Quote[] = [];
-    const result = await fetch(`https://usu-quotes-mimic.vercel.app/api/search?query=${author}`);
-    const json = await result.json();
-    for (const i of await json.results) {
-      const quote: Quote = {
-        _id: i._id,
-        content: i.content,
-        author: i.author,
-      };
-      quoteList.push(quote);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/users/:id",
+                element: <User />
+            },
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    },
+    {
+        path: 'home/room',
+        element: <h1> i am on the room page </h1>
+    },
+    {
+        path: 'users/:id',
+        element: <h1> i am on the id search page </h1>
     }
-    setQuotes(quoteList);
-  }
+]);
 
-  async function loadRandom() {
-    const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
-    setQuote(await result.json());
-  }
+export const App = () => {
 
-  useEffect (() => {
-    loadRandom();
-  }, []);
+    const [api, setApi] = useState( new Api() )
+    //api available to any child of component 
 
-  function SearchResults(){
-    return (quotes.map((q) => (
-        <div key={q._id} className="quote">
-          {q.content}
-          <div>-{q.author}</div>
-        </div>
-      )));
-  }
-
-  return(
-    <div>
-      <form className={className}>
-        <h2> The Quote Searcher-ma-tron </h2>
-        <br></br>
-        <input
-          type='text'
-          placeholder='search for a quote'
-          value={author}
-          onChange={e => setAuthor(e.target.value)}
-        />
-        <br></br>
-        <button 
-          className='cool'
-          type='submit'
-          onClick={search}>Search
-        </button>
-      </form>
-      <div id="outer-div" className='container'>
-        {hasSearched ? SearchResults() : <div className='quote'>{quote?.content} <br></br>-{quote?.author}</div>} 
-      </div>
-    </div>
-    );
+    return (
+    <>
+        <ApiContext.Provider value={api}> 
+            <RouterProvider router={router} />
+        </ApiContext.Provider>
+    </>
+    )
 }
